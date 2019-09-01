84e0c46b37f09fcf2e29f1706e34ffc593b6c430
Koichi HARUNA <koichi.h+github@gmail.com>
2019-09-02 01:35:59 +0900

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,22 +1,20 @@
-# rbenv replased by fisherman
-#eval "$(rbenv init -)";
-#set -x PATH $HOME/.rbenv/bin $PATH
-#set -x PATH $HOME/.rbenv/shims $PATH
-#rbenv rehash >/dev/null ^&1
+# pyenv
+set -x PYENV_ROOT $HOME/.pyenv
+set -x PATH $PYENV_ROOT/bin $PATH
+source (pyenv init - | psub)
+
+# rbenv
+set -x PATH $HOME/.rbenv/bin $PATH
+source (rbenv init - | psub)
 
 set -x LC_LANG ja_JP.UTF-8
 set -x LC_CTYPE ja_JP.UTF-8
 
-# pyenv replased by fisherman
-# export PATH="$HOME/.pyenv/shims:$PATH" 
-#set -x PATH $HOME/.pyenv/bin $PATH
-#status --is-interactive; and . (pyenv init -| psub)
-
-
 #set fish_plugins git tmux 
-powerline-daemon -q
-. ~/.pyenv/versions/2.7.13/lib/python2.7/site-packages/powerline/bindings/fish/powerline-setup.fish
-powerline-setup
+# powerline-go
+function fish_prompt
+    /usr/local/bin/powerline-go -error $status -shell bare
+end
 
 # peco
 function fish_user_key_bindings
