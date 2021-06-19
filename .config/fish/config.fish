9bb28c0a8be20ea0f711ca978e93c44650c2c460
Koichi HARUNA <koichi.h+github@gmail.com>
2021-06-19 14:52:11 +0900

    feat: update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,17 +7,36 @@ source (pyenv init - | psub)
 set -x PATH $HOME/.rbenv/bin $PATH
 source (rbenv init - | psub)
 
-set -x LC_LANG ja_JP.UTF-8
-set -x LC_CTYPE ja_JP.UTF-8
+# nodeenv
+set -x PATH $HOME/.nodenv/bin $PATH
+source (nodenv init - | psub)
 
-#set fish_plugins git tmux 
+#fisherパッケージoh-my-fish/plugin-pecoの設定
+function fish_user_key_bindings
+  bind \cr peco_select_history # Bind for prco history to Ctrl+r
+end
+
+
 # powerline-go
 function fish_prompt
     /usr/local/bin/powerline-go -error $status -shell bare
 end
 
-# peco
-function fish_user_key_bindings
-  bind \cr 'peco_select_history (commandline -b)'
-end
+# go/bin
+set -x PATH $HOME/go/bin $PATH
 
+# rust
+set -x PATH $HOME/.cargo/bin $PATH
+
+set -x LC_LANG ja_JP.UTF-8
+set -x LC_CTYPE ja_JP.UTF-8
+
+# asdf
+source ~/.asdf/asdf.fish
+
+# github cli
+eval (gh completion -s fish| source)
+
+# sdkman
+set -x JAVA_HOME $HOME/.sdkman/candidates/java/current/
+set -x PATH $PATH $JAVA_HOME/bin
